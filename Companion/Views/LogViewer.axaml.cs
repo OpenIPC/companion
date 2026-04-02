3a45dc547a8a09933862aad938de70d1dc22e4ed
Mike Carr <mcarr67@gmail.com>
2026-04-02 11:24:14 -0700

    Improve log viewer latest-follow behavior

--- Companion/Views/LogViewer.axaml.cs
+++ Companion/Views/LogViewer.axaml.cs
@@ -1,4 +1,7 @@
+using System.Collections.Specialized;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using Companion.ViewModels;
 
@@ -6,11 +9,93 @@ namespace Companion.Views;
 
 public partial class LogViewer : UserControl
 {
+    private const double BottomThreshold = 24;
+    private LogViewerViewModel? _currentViewModel;
+
     public LogViewer()
     {
         InitializeComponent();
 
         //if (!Design.IsDesignMode) DataContext = new LogViewerViewModel();
         DataContext = App.ServiceProvider.GetService<LogViewerViewModel>();
+
+        DataContextChanged += OnDataContextChanged;
+        DetachedFromVisualTree += (_, _) => UnsubscribeFromLogMessages(_currentViewModel);
+    }
+
+    private void OnDataContextChanged(object? sender, System.EventArgs e)
+    {
+        UnsubscribeFromLogMessages(_currentViewModel);
+
+        if (DataContext is LogViewerViewModel viewModel)
+        {
+            _currentViewModel = viewModel;
+            viewModel.PropertyChanged += ViewModelOnPropertyChanged;
+            viewModel.LogMessages.CollectionChanged += OnLogMessagesCollectionChanged;
+            if (viewModel.FollowLatest)
+                ScrollToLatest();
+        }
+        else
+        {
+            _currentViewModel = null;
+        }
+    }
+
+    private void ViewModelOnPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (sender is not LogViewerViewModel viewModel)
+            return;
+
+        if (e.PropertyName == nameof(LogViewerViewModel.FollowLatest) && viewModel.FollowLatest)
+            ScrollToLatest();
+    }
+
+    private void OnLogMessagesCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (DataContext is not LogViewerViewModel viewModel || e.Action != NotifyCollectionChangedAction.Add)
+            return;
+
+        if (viewModel.FollowLatest)
+        {
+            ScrollToLatest();
+            return;
+        }
+
+        viewModel.NotifyMessageArrivedWhileDetached();
+    }
+
+    private void OnScrollChanged(object? sender, ScrollChangedEventArgs e)
+    {
+        if (DataContext is not LogViewerViewModel viewModel)
+            return;
+
+        if (IsNearBottom())
+            viewModel.NotifyAttachedToLatest();
+        else
+            viewModel.NotifyDetachedFromLatest();
+    }
+
+    private bool IsNearBottom()
+    {
+        var remaining = LogScrollViewer.Extent.Height - LogScrollViewer.Viewport.Height - LogScrollViewer.Offset.Y;
+        return remaining <= BottomThreshold;
+    }
+
+    private void ScrollToLatest()
+    {
+        Dispatcher.UIThread.Post(() =>
+        {
+            var targetY = LogScrollViewer.Extent.Height;
+            LogScrollViewer.Offset = new Vector(LogScrollViewer.Offset.X, targetY);
+        }, DispatcherPriority.Background);
+    }
+
+    private void UnsubscribeFromLogMessages(LogViewerViewModel? viewModel)
+    {
+        if (viewModel is null)
+            return;
+
+        viewModel.PropertyChanged -= ViewModelOnPropertyChanged;
+        viewModel.LogMessages.CollectionChanged -= OnLogMessagesCollectionChanged;
     }
 }
